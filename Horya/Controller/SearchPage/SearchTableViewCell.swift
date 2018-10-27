84e3c390abdcf790463484bfaa04897d6031ebd9
omekuzco <omekuzco@gmail.com>
2018-10-28 00:28:21 +0700

    update Search Bar v.0.0.3

---
+++ Horya/Controller/SearchPage/SearchTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SearchTableViewCell.swift
+//  Horya
+//
+//  Created by Mac on 27/10/2561 BE.
+//  Copyright © 2561 Mac. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var nameLbl: UILabel!
+    @IBOutlet var categoryLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
